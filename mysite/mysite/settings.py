62b78d7324d53a776cf5b653e1b74ef33b083709
Rahul Bhandari <mr.bhandari@gmail.com>
2014-06-15 16:16:01 -0700

    update for doctors for text messages

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -13,10 +13,11 @@ PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_ROOT, 'pdb.db'),                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',
+            #os.path.join(PROJECT_ROOT, 'mysql.db'),                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'thebakery',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -8,10 +8,10 @@ from product_data.models import *
 
 
 def search(request):
-    if 'q' in request.GET or 'brand' in request.GET or 'title' in request.GET:
-        if 'q' in request.GET and request.GET['q']:
-            q = request.GET['q']
-            print q
+    if 'hcpcs' in request.GET or 'state' in request.GET:
+        if 'hcpcs' in request.GET and request.GET['hcpcs']:
+            hcpcs = request.GET['hcpcs']
+            print hcpcs
             products = Product.objects.filter(description__icontains=q)
             print products[1].brand
             print len(products)
@@ -32,26 +32,26 @@ def search(request):
              #'query': q
              })
     else:
-        error = "Please enter text in atleast one of the boxes below."
+        error = "Welcome"
         return render_to_response('search_form.html',
                                   {'error':error})
 
 
 def hello(request):
 	return HttpResponse("Hello world")
-
-def current_datetime(request):
-	now = datetime.datetime.now()
-	t = get_template('current_datetime.html')
-	html = t.render(Context({'current_date':now}))
-	return HttpResponse(html)
-
-def hours_ahead(request, offset):
-	try:
-		offset = int(offset)
-	except ValueError:
-		raise Http404()
-	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-	assert False
-	html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-	return HttpResponse(html)
+#
+#def current_datetime(request):
+#	now = datetime.datetime.now()
+#	t = get_template('current_datetime.html')
+#	html = t.render(Context({'current_date':now}))
+#	return HttpResponse(html)
+#
+#def hours_ahead(request, offset):
+#	try:
+#		offset = int(offset)
+#	except ValueError:
+#		raise Http404()
+#	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+#	assert False
+#	html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+#	return HttpResponse(html)
